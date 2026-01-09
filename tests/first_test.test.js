49920460c2ab3f08c5d8fc089aaf6f123daf4fa5
Shebin P Biju(UST <268838@ust.com>
2026-01-09 19:32:56 +0530

    expect field updated in first_test.test.js

--- tests/first_test.test.js
+++ tests/first_test.test.js
@@ -4,7 +4,7 @@ import { test, expect } from '@playwright/test';
 test('my first program', async ({ page })=> {
     await page.goto('https://www.google.com/');
     //check title
-    await expect(page).toHaveTitle(/Google/)
+    await expect(page).toHaveTitle('Google')
 });
 
 
