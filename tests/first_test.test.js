240561d136b18bb8370006afd17a6a071f87e1e6
Shebin P Biju(UST <268838@ust.com>
2026-01-09 19:28:44 +0530

    first program updated

---
+++ tests/first_test.test.js
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+// first test
+test('my first program', async ({ page })=> {
+    await page.goto('https://www.google.com/');
+    //check title
+    await expect(page).toHaveTitle(/Google/)
+});
+
+
