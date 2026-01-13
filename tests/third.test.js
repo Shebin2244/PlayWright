c50048e798a36cb05121be8dc9e632efc503e380
Shebin P Biju(UST <268838@ust.com>
2026-01-13 16:56:18 +0530

    new codegen code added

---
+++ tests/third.test.js
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('login test', async ({ page }) => {
+  await page.goto('https://practice.expandtesting.com/login');
+  await page.locator('.card-body').click();
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill('practice');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('SuperSecretPassword!');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole('link', { name: 'Logout' }).click();
+});
+
+
+//npx playwright codegen  --browser=chromium --channel=msedge https://practice.expandtesting.com/login
