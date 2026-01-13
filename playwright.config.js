3aea901ce01706946eb8b57792d1bc769b7d0507
Shebin P Biju(UST <268838@ust.com>
2026-01-13 17:21:10 +0530

    trace report and doc file updated

--- playwright.config.js
+++ playwright.config.js
@@ -5,10 +5,11 @@ export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  // retries: process.env.CI ? 2 : 0,
+  retries:1,
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
-
+  timeout:5 * 1000,
   use: {
     trace: 'on-first-retry',
   },
--- tests/third.test.js
+++ tests/third.test.js
@@ -5,7 +5,7 @@ test('login test', async ({ page }) => {
   await page.locator('.card-body').click();
   await page.getByRole('textbox', { name: 'Username' }).click();
   await page.getByRole('textbox', { name: 'Username' }).fill('practice');
-  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Passwords' }).click();
   await page.getByRole('textbox', { name: 'Password' }).fill('SuperSecretPassword!');
   await page.getByRole('button', { name: 'Login' }).click();
   await page.getByRole('link', { name: 'Logout' }).click();
